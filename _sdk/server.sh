892fb167be2b5616485563a2fc5ca771d35f02c4
CheeseCrustery <xxmaxmustermannxx@gmx.de>
2022-03-09 19:12:34 +0100

    telemetry fixes

---
+++ _sdk/server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./venv/lib/python3.9/site-packages/mavsdk/bin/mavsdk_server serial:///dev/ttyAMA0
--- _sdk/telemetry.py
+++ _sdk/telemetry.py
@@ -8,26 +8,30 @@ class Mav():
 		self.__mav = mavsdk.System()
 		logging.info("created system")
 		self.battery = 1.0 # 0.0 - 1.0
-		self.pos = [0.0, 0.0] # lat, lon
-	
+		self.position = [0.0, 0.0] # lat, lon
+
 	async def __get_battery(self):
-		async for val in self.__mav.telemetry.battery:
+		async for val in self.__mav.telemetry.battery():
 			logging.info(f"received battery: {val}")
 			self.battery = val.remaining_percent
 			logging.info(f"set battery to {self.battery}")
+		logging.warning("battery out")
 
 	async def __get_pos(self):
-		async for val in self.__mav.telemetry.pos:
+		async for val in self.__mav.telemetry.position():
 			logging.info(f"received pos {val}")
-			self.pos = [val.latitude_deg, val.longitude_deg]
+			self.position = [val.latitude_deg, val.longitude_deg]
 			logging.info(f"set pos to {self.pos}")
+		logging.warning("pos out")
 
 	async def keep_connected(self):
 		logging.info("connecting...")
-		await self.__mav.connect(system_address='serial:///dev/ttyAMA0')
-		logging.info("connected")
-		await self.__mav.param.set_param_int('COM_RC_IN_MODE', 2)
-		await asyncio.gather(self.__get_battery, self.__get_pos)
+		out = await self.__mav.connect(system_address='serial:///dev/ttyAMA0')
+		logging.info(f"connected: {out}")
+		v = await self.__mav.info.get_version()
+		logging.info(v)
+		#await self.__mav.param.set_param_int('COM_RC_IN_MODE', 2)
+		await asyncio.gather(self.__get_battery(), self.__get_pos())
 		logging.info("disconnecting")
 
 async def main():
